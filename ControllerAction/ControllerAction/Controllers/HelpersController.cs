54dcc2a26733424f007615f8884a5a3af425255f
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-02 20:13:25 +0100

    W trakcie

---
+++ ControllerAction/ControllerAction/Controllers/HelpersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerAction.Models;
+
+namespace ControllerAction.Controllers
+{
+    public class HelpersController : Controller
+    {
+        // GET: Helpers
+        public ActionResult Index()
+        {
+            var result = new HelpersModel()
+            {
+                Bool = true,
+                DateTime = new DateTime(2000, 01, 02),
+                Decimal = 12321321.333m,
+                String = "Przykład helperów w ASP.NET MVC"
+            };
+
+
+
+            return View(result);
+        }
+
+    }
+    }
---
+++ ControllerAction/ControllerAction/Models/HelpersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControllerAction.Models
+{
+    public class HelpersModel
+    {
+        public string String { get; set; }
+        public decimal Decimal { get; set; }
+        public bool Bool { get; set; }
+        [UIHint("DateTimePicker")]
+        public DateTime DateTime { get; set; }
+    }
+}
