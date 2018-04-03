5303b3e2f697b624d01e753cc0aa430a2b46c353
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-03 16:41:45 +0100

    W trakcie

---
+++ AjaxDemo/AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AjaxDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string searchQuery = null)
+        {
+            IEnumerable<Person> personList;
+
+            if (searchQuery !=null)
+            {
+                personList = Person.GetPersonList().Where(p => p.FirstName.Contains(searchQuery) || p.LastName.Contains(searchQuery) || searchQuery == p.FirstName + " " + p.LastName).ToArray();
+            }
+            else
+            {
+                personList = Person.GetPersonList().ToArray();
+            }
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_PersonList", personList);
+            }
+
+            return View(personList);
+        }
+
+        public ActionResult PersonSuggestions(string term)
+        {
+            var personList = Person.GetPersonList().Where(p => p.FirstName.Contains(term) || p.LastName.Contains(term)).Take(5).Select(p => new { label = p.FirstName + " " + p.LastName });
+            return Json(personList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxDemo/AjaxDemo/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxDemo.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public static IEnumerable<Person> GetPersonList()
+        {
+            var persons = new List<Person>
+            {
+                new Person() { FirstName="Jan", LastName="Kowalski"},
+                new Person() { FirstName="Krzysztof", LastName="Nowak"},
+                new Person() { FirstName="Mariusz", LastName="Brzeczysz"},
+                new Person() { FirstName="Janusz", LastName="Marecki"},
+                new Person() { FirstName="Katarzyna", LastName="Złotowska"},
+                new Person() { FirstName="Julian", LastName="Lubosiński"},
+                new Person() { FirstName="Grzegorz", LastName="Rogowski"},
+                new Person() { FirstName="Bartłomiej", LastName="Zasiński"},
+                new Person() { FirstName="Jan", LastName="Deweloperski"},
+                new Person() { FirstName="Michał", LastName="Kleczkowy"},
+                new Person() { FirstName="Karol", LastName="Świtała"},
+            };
+            return persons;
+            
+        }
+    }
+}
