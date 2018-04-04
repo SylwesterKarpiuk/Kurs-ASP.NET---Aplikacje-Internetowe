4f4126048cf2c600b1a161cb13bbe84ce1bed8cf
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-04 21:21:42 +0100

    W trakcie

---
+++ ContactFormDemo/ContactFormDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ContactFormDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactFormDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddQuestion(Question question)
+        {
+            if (question.QuestionText=="?")
+            {
+                ModelState.AddModelError("QuestionText", "Komunikat dodany manualnie. Nie może być samego znak zapytania...");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("Index", question);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
