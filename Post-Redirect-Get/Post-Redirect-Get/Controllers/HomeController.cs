fd316fc18c810a156c4295ed063c7ec2c7c6b0f2
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-08 20:04:07 +0100

    W trakcie

--- Post-Redirect-Get/Post-Redirect-Get/Controllers/HomeController.cs
+++ Post-Redirect-Get/Post-Redirect-Get/Controllers/HomeController.cs
@@ -49,11 +49,36 @@ namespace Post_Redirect_Get.Controllers
             {
                 counters.AplicationCounter = 0;
             }
+
+            if (Session["counter"] != null)
+            {
+                counters.SessionCounter = (int)Session["counter"];
+            }
+            else
+            {
+                counters.SessionCounter = 0;
+            }
+
+            if(Request.Cookies["counter"] != null)
+            {
+                counters.CookieCounter = int.Parse(Request.Cookies["counter"].Value);
+            }
+            else
+            {
+                counters.CookieCounter = 0;
+            }
             return counters;
         }
         private void SetCounters(Counters counter)
         {
             HttpContext.Application["counter"] = counter.AplicationCounter;
+
+            Session["counter"] = counter.SessionCounter;
+
+            HttpCookie cookie = new HttpCookie("counter", counter.CookieCounter.ToString());
+            //cookie.Expires = DateTime.Now.AddDays(-1);
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.SetCookie(cookie);
         }
         [HttpPost]
         public ActionResult IncrementApplication()
@@ -63,6 +88,27 @@ namespace Post_Redirect_Get.Controllers
             counters.AplicationCounter++;
             SetCounters(counters);
             HttpContext.Application.UnLock();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult IncrementSession()
+        {
+            var counters = GetCounters();
+
+            counters.SessionCounter++;
+
+            SetCounters(counters);
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult IncrementCookies()
+        {
+            var counters = GetCounters();
+            counters.CookieCounter++;
+            SetCounters(counters);
 
             return RedirectToAction("Index");
         }
