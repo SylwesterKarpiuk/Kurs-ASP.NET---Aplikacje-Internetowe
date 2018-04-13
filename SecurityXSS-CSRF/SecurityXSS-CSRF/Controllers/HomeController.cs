7987beeae61be4c207685b6462afd11bcd3236e1
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-13 14:04:54 +0100

    Revert "Revert "W trakcie""
    This reverts commit f074441a2d609ab280f98b5162a3d6dbc1d3f9cd.

---
+++ SecurityXSS-CSRF/SecurityXSS-CSRF/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Security.Application;
+using SecurityXSS_CSRF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityXSS_CSRF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(HomeController.Questions);
+        }
+
+        private static List<Question> Questions { get; set; }
+
+        static HomeController()
+        {
+            HomeController.Questions = new List<Question>();
+        }
+
+        [ValidateInput(false)]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddQuestion(Question newQuestion)
+        {
+            newQuestion.QuestionText = Sanitizer.GetSafeHtmlFragment(newQuestion.QuestionText);
+
+            HomeController.Questions.Add(newQuestion);
+            return RedirectToAction("Index");
+        }
+    }
+}
