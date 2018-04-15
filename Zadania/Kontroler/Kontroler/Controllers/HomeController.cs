dd92bfafa19e8b0feb92965ef5f6c0292c2e2dde
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-15 14:33:05 +0100

    W trakcie

---
+++ Zadania/Kontroler/Kontroler/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Kontroler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kontroler.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void SendEmail(Data data)
+        {
+            Response.Write(string.Format("Email: {0}, Temat: {1}, Treść: {2}", data.Email, data.Email, data.Tresc));
+        }
+
+    }
+}
